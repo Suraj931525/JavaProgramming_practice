9d0d7f08ac9bd82f9c4a7b1f999d68d7d82c415c
Suraj931525 <surajpatil9315@gmail.com>
2026-06-25 20:33:01 +0530

    add task is related in finally block

---
+++ src/Finallyblock/demo2.java
@@ -0,0 +1,19 @@
+package Finallyblock;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=0;
+		try {
+			int c=a/b;
+			System.out.println(c);
+		}
+		
+		
+		finally {
+			System.out.println("Finally block will be always executes");
+		}
+	}
+
+}
